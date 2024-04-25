e1ee47893f1aeb0041d3ed050f0a3cd383aae9f3
YanaiShinsuke <yanai.shinsuke@brains-tech.co.jp>
2024-04-25 17:21:51 +0900

    Add database item, and add login to database script

--- yanai_work/src/submit_form.php
+++ yanai_work/src/submit_form.php
@@ -1,8 +1,20 @@
 <?php
 // submit_form.php
+try {
+    # 接続用変数を定義
+    $host = 'mysql';
+    $dbname = $_ENV['MYSQL_DATABASE'];
+    $user = 'root';
+    $password = $_ENV['MYSQL_ROOT_PASSWORD'];
+
+    # 新しいPDOオブジェクトを作成し、MySQLデータベースに接続
+    $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8mb4", $user, $password);
+}   catch (PDOException $e) {
+    echo "データベース接続エラー　:".$e->getMessage();
+}
 
 if ($_SERVER["REQUEST_METHOD"] === "GET") {
-    // Retrieve form data
+    // index.phpで入力されたデータを取得
     $name = $_GET["name"];
     $email = $_GET["email"];
     $message = $_GET["message"];
@@ -15,6 +27,13 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
 
     // Process the form data
     // ... (Your processing logic here)
+    $sql = "INSERT INTO question_table (name, email, message) VALUES (:name, :email, :message)";
+    $stmt = $db->prepare($sql);
+
+    // 挿入する値を配列に格納
+    $params = array(':name' => $name, ':email' => $email, ':message' => $message);
+    // SQLの実行
+    $stmt->execute($params);
 
     // Redirect to the thank you page
     header('Location: thankyou.php');
