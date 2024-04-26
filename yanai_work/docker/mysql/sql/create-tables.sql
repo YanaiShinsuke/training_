7cf30c356a89684cac9c734e8898a45b018fc925
YanaiShinsuke <yanai.shinsuke@brains-tech.co.jp>
2024-04-26 14:37:19 +0900

    change length

--- yanai_work/docker/mysql/sql/create-tables.sql
+++ yanai_work/docker/mysql/sql/create-tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE `question_table`
 (
   `id`       INT(10) AUTO_INCREMENT,
   `name`     VARCHAR(30) NOT NULL,
-  `email`    VARCHAR(60) NOT NULL,
-  `message`  VARCHAR(200) NOT NULL,
+  `email`    VARCHAR(100) NOT NULL,
+  `message`  VARCHAR(500) NOT NULL,
     PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
